366ca6e97b88150a4c1538ceaabeb605ecb3aa75
levy310898 <levy310898@gmail.com>
2021-07-07 16:08:08 +0700

    add folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import React,{Suspense} from 'react';
+import {BrowserRouter,Route,Switch,Redirect,Link} from 'react-router-dom';
 import './App.css';
+import NotFound from './components/NotFound/index';
 
+const Photo = React.lazy(() => import('./features/Photo/index'))
 function App() {
+  
   return (
     <div className="App">
-      Photo app
+      <Suspense fallback = {<div>Loading ...</div>}>
+        <BrowserRouter>
+          
+          <Switch>
+            <Redirect exact from='/' to='/photos' />
+            <Route path="/photos" component={ Photo} />
+            <Route component={NotFound}/>
+          </Switch>
+        </BrowserRouter>
+      </Suspense>
     </div>
   );
 }

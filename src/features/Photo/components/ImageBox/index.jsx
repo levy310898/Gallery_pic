264927707ec408d632965e05d2f5ac09fabcc5c9
levy310898 <levy310898@gmail.com>
2021-07-16 13:13:27 +0700

    add save button and end this project. will comeback later :v

--- src/features/Photo/components/ImageBox/index.jsx
+++ src/features/Photo/components/ImageBox/index.jsx
@@ -1,8 +1,9 @@
-import React,{useEffect} from 'react'
+import React,{useState,useEffect} from 'react'
 import './ImageBox.scss';
+import { Button } from 'antd';
 function ImageBox(props) {
   const { id, author, width, height, download_url } = props.data;
-  
+  const [saveImage, setSaveImage] = useState(false);
   // useEffect(() => {
   //   console.log(`get height image ${id} = `, getHeight());
   // }, []);
@@ -12,7 +13,8 @@ function ImageBox(props) {
   }
   return (
     <div className="homepage__card" style = {{height:`${getHeight()}px`,width:'250px'}}>
-      <img src={download_url} alt="" style={{ width: '100%', height: '100%' }} className= 'homepage__card__image'/>
+      <img src={download_url} alt="" style={{ width: '100%', height: '100%' }} className='homepage__card__image' />
+      <Button type={saveImage ? "primary" : "danger"} className="homepage__card__btn" onClick={() => setSaveImage(prev => !prev)}>{saveImage?'Saved':'Save'}</Button>
     </div>
   )
 }
